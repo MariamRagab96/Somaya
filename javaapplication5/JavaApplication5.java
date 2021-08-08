1ae7435b26444cb27cd90861c0d240a90e898f22
MariamRagab96 <mariamragab.ieee@gmail.com>
2021-08-08 06:41:25 -0700

    first

---
+++ javaapplication5/JavaApplication5.java
@@ -0,0 +1,15 @@
+
+package javaapplication5;
+
+import java.util.Scanner;
+
+
+public class JavaApplication5 {
+
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(sc.nextInt()+sc.nextInt());
+    }
+    
+}
