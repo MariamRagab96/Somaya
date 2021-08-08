46264db955d388e9cde6466cad0e6ac531c77233
SomayaRagab <eng.somayaragab@gmail.com>
2021-08-08 08:57:18 +0200

    fourth

--- javaapplication5/JavaApplication5.java
+++ javaapplication5/JavaApplication5.java
@@ -10,7 +10,7 @@ public class JavaApplication5 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
        
-         System.out.println(sc.nextInt()+sc.nextInt()+sc.nextInt());
+         System.out.println(sc.nextInt()*sc.nextInt()+sc.nextInt());
     }
     
 }
